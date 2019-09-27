e451e5d552b1fb886f86dd1b2fd2b648166bd421
GreatSnoopy <GreatSnoopy@gmail.com>
2019-09-27 15:22:56 +0300

    switch back to calling len directly

--- leaderboard.go
+++ leaderboard.go
@@ -12,15 +12,13 @@ type LeaderBoard []*Item
 
 //Collect adds an item to a leaderboard, at the proper position
 func (lb *LeaderBoard) Collect(newItem Item) {
-	lblen := len(*lb)
-	lbcap := cap(*lb)
-	if lblen == lbcap && newItem.LessThan(*(*lb)[lblen-1]) {
+	if len(*lb) == cap(*lb) && newItem.LessThan(*(*lb)[len(*lb)-1]) {
 		//if leaderboard is full and the new item is smaller than the last item in leaderboard, we can just discard it
 		return
 	}
 	for i, item := range *lb {
 		if (*item).LessThan(newItem) {
-			if lblen < lbcap {
+			if len(*lb) < cap(*lb) {
 				*lb = append(*lb, nil)
 			}
 			copy((*lb)[i+1:], (*lb)[i:])
@@ -29,7 +27,7 @@ func (lb *LeaderBoard) Collect(newItem Item) {
 		}
 	}
 
-	if lblen < lbcap {
+	if len(*lb) < cap(*lb) {
 		*lb = append(*lb, &newItem)
 		return
 	}
